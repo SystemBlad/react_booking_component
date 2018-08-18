7175bd41ab5736fc97a6a56c7f2aeba5caea1703
user2 <jaimelunaec@hotmail.com>
2018-08-18 00:07:42 -0500

    Number components

--- src/components/booking-mask/booking-mask.js
+++ src/components/booking-mask/booking-mask.js
@@ -3,6 +3,7 @@ import { Button, Form, FormGroup, Label, Input, Dropdown, DropdownToggle, Dropdo
 import '../react-datetime/react-datetime.css';
 import IataCodeInput from '../iata-code-input/IataCodeInput';
 import Datetime from 'react-datetime';
+import NumericInput from 'react-numeric-input';
 class BookingMask extends Component {
     constructor(props) {
         super(props);
@@ -26,31 +27,66 @@ class BookingMask extends Component {
             <div className="container">
                 <Form>
                     <FormGroup>
-                        <Label for="exampleEmail">From:</Label>
-                        <IataCodeInput/>
+                        <Label for="exampleEmail">
+                            From:
+                            <IataCodeInput/>
+                        </Label>
                     </FormGroup>
                     <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
-                        <Label for="examplePassword">To:</Label>
-                        <IataCodeInput/>
+                        <Label for="examplePassword">
+                            To:
+                            <IataCodeInput/>
+                        </Label>
                     </FormGroup>
                     <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
-                        <Label for="examplePassword">Outbound Flight</Label>
-                        <Datetime closeOnSelect={true}/>
+                        <Label for="examplePassword">
+                            Outbound Flight:
+                            <Datetime closeOnSelect={true} dateFormat="YYYY-MM-DD" timeFormat={false}/>
+                        </Label>
                     </FormGroup>
                     <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
-                        <Label for="examplePassword">Return Flight:</Label>
-                        <Datetime closeOnSelect={true}/>
+                        <Label for="examplePassword">
+                            Return Flight:
+                            <Datetime closeOnSelect={true} dateFormat="YYYY-MM-DD" timeFormat={false}
+                                      inputProps={{ placeholder: 'YYYY-MM-DD' }}/>
+                        </Label>
                     </FormGroup>
                     <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
-                        <Label for="examplePassword">Passengers:</Label>
                         <label>
-                            Pick your favorite flavor:
-                            <select value={this.state.value} onChange={this.handleChange}>
-                                <option value="grapefruit">Grapefruit</option>
-                                <option value="lime">Lime</option>
-                                <option value="coconut">Coconut</option>
-                                <option value="mango">Mango</option>
-                            </select>
+                            Adults:
+                            <NumericInput className="form-control" min={0} max={20} value={50}
+                                          style={{
+                                              input: {
+                                                  width: 60
+                                              }
+                                          }}
+                            />
+                        </label>
+                        <label>
+                            Minors:
+                            <NumericInput className="form-control" min={0} max={20} value={50}
+                                          style={{
+                                              input: {
+                                                  width: 60
+                                              }
+                                          }}
+                            />
+                        </label>
+                        <label>
+                            Infants:
+                            <NumericInput className="form-control" min={0} max={20} value={50}
+                                          style={{
+                                              input: {
+                                                  width: 60
+                                              }
+                                          }}
+                            />
+                        </label>
+                        <label>
+                            Total:
+                            <label style={{color:"red"}} className="form-control">
+                                70
+                            </label>
                         </label>
                     </FormGroup>
                     <Button>Search</Button>
